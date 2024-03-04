81d73c8d124b7a54a9976c4b569d46b0c5b690d2
Asude çetin <220202060@kocaeli.edu.tr>
2024-03-05 02:05:36 +0300

    drawing triangles with c#
    drawing triangles with c#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Üçgenin boyutunu girin: ");
+        int boyut = Convert.ToInt32(Console.ReadLine());
+
+        DrawTriangle(boyut);
+    }
+
+    static void DrawTriangle(int size)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            // Boşlukları ekleyerek üçgeni ortalamak için
+            for (int j = 1; j <= size - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Yıldızları çizmek
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine(); // Bir sonraki satıra geçmek için
+        }
+    }
+}
